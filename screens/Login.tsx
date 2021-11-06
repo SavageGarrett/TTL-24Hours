581c2493ab09ad97ae87d4b149f2762a0bfe8e16
SavageGarrett <carder.320@osu.edu>
2021-11-06 10:37:22 -0400

    Move Main Navigation to Another Component

---
+++ screens/Login.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  ScrollView,
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+import { Icon, ProfileItem } from "../components";
+import DEMO from "../assets/data/demo";
+import styles, { WHITE } from "../assets/styles";
+
+/* Auth0 Login Component Screen */
+const Login = () => {
+
+  return (
+    <ImageBackground
+      source={require("../assets/images/bg.png")}
+      style={styles.bg}
+    >
+      
+    </ImageBackground>
+  );
+};
+
+export default Login;
